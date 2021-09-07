6b16a67fb926ea61e6d2a936d244a2834925347b
Vítor <Vtor@192.168.15.9>
2021-09-07 18:53:31 -0300

    Atualizando projeto de calculo de nota alunos

---
+++ src/notas/Notas.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notas;
+
+/**
+ *
+ * @author Vítor
+ */
+public class Notas {
+    private double nota;
+    private String nome;
+    private String situacao;
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+    
+}
